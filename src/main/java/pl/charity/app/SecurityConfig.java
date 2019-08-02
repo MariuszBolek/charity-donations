1ebedebef1ef2073607b7dedb77d697b0c1eaa8f
Mariusz Bolek <mariusz.bolek91@gmail.com>
2019-08-02 11:08:38 +0200

    ref: spring security refactoring

--- src/main/java/pl/charity/app/SecurityConfig.java
+++ src/main/java/pl/charity/app/SecurityConfig.java
@@ -1,3 +1,5 @@
+package pl.charity.app;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
